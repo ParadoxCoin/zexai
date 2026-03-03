005edde6530a0fbe7a1c51c6e346ccccdb454414
Paradox Coin <86842413+ParadoxCoin@users.noreply.github.com>
2026-03-04 01:48:06 +0300

    fix: updated global.d.ts to target react module for custom web3 elements

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,8 +1,12 @@
-declare namespace JSX {
-    interface IntrinsicElements {
-        'w3m-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        'w3m-network-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        'w3m-connect-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        'w3m-account-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+import * as React from 'react';
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'w3m-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+            'w3m-network-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+            'w3m-connect-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+            'w3m-account-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        }
     }
 }
